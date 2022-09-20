6abf1c562a77af266469e47aef62599ad2e06c78
Vlad Belko <vladbelko2002@gmail.com>
2022-09-21 00:25:20 +0300

    Add new view & controller (Hello world)

--- Web_053504_Belko/Startup.cs
+++ Web_053504_Belko/Startup.cs
@@ -35,6 +35,7 @@ namespace Web_053504_Belko
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
