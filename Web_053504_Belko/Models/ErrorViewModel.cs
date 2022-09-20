6b757c0f316e27f17fd534fcb988bfd8199cb511
Vlad Belko <vladbelko2002@gmail.com>
2022-09-20 23:05:21 +0300

    Create project for 1st lab

---
+++ Web_053504_Belko/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Web_053504_Belko.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
